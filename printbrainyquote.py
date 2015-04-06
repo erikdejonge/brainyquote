4b8ce0eaba77d99cfc6d10cbe0bb59775a1eef46
Erik de Jonge <erik@a8.nl>
2015-04-06 13:59:18 +0200

    guestbook Monday 06 April 2015 (week:14 day:96), 13:59:18

--- printbrainyquote.py
+++ printbrainyquote.py
@@ -1,58 +1,51 @@
 #!/usr/local/bin/python3
-
 # coding=utf-8
-# $Id: f95776c6a6fa8a2b954005004fce99819efdee4f $
 """
 fortunes
 """
-from __future__ import print_function
-from __future__ import unicode_literals
-from __future__ import division
-from __future__ import absolute_import
-from builtins import open
+from __future__ import division, print_function, absolute_import, unicode_literals
 from future import standard_library
-standard_library.install_aliases()
-from clint.textui.progress import mill
-
-# ---------------------------------------------------------------------------
-# Imports
-# ---------------------------------------------------------------------------
 
 import os
 import sys
+import pickle
 import random
-import pickle as pickle
 
-# ---------------------------------------------------------------------------
-# Exports
-# ---------------------------------------------------------------------------
+from argparse import ArgumentParser
+from clint.textui.progress import mill
+
 __all__ = ['main', 'get_random_fortune', 'make_fortune_data_file']
-
-# Info about the module
-__version__ = '1.0'
-__author__ = 'Brian M. Clapper'
-__email__ = 'bmc@clapper.org'
 __url__ = 'http://software.clapper.org/fortune/'
 __copyright__ = '2008-2011 Brian M. Clapper'
-__license__ = 'BSD-style license'
+__author__ = 'Brian M. Clapper'
+__email__ = 'bmc@clapper.org'
+__version__ = '1.0'
 
-# ---------------------------------------------------------------------------
-# Internal Constants
-# ---------------------------------------------------------------------------
 _PICKLE_PROTOCOL = 2
 
-# ---------------------------------------------------------------------------
-# Functions
-# ---------------------------------------------------------------------------
 
+def _read_fortunes(fortune_file):
+    """ Yield fortunes as lists of lines """
+    result = []
+    start = None
+    pos = 0
 
-def random_int(start, end):
-    """
-    @type start: str, unicode
-    @type end: str, unicode
-    @return: None
-    """
-    return random.randint(start, end)
+    for line in fortune_file:
+        if line == "%\n":
+            if pos != 0:  # "%" at top of file. Skip it.
+                yield (start, pos - start, result)
+                result = []
+                start = None
+        else:
+            if start is None:
+                start = pos
+
+            result.append(line)
+
+        pos += len(line)
+
+    if result:
+        yield (start, pos - start, result)
 
 
 def get_random_fortune(fortune_file):
@@ -68,12 +61,24 @@ def get_random_fortune(fortune_file):
     fortune_index = open(fortune_index_file, "rb")
     data = pickle.load(fortune_index)
     fortune_index.close()
-    random_record = random_int(0, len(data) - 1)
-    (start, length) = data[random_record]
-    f = open(fortune_file, 'r')
-    f.seek(start)
-    fortune_cookie = f.read(length)
-    f.close()
+    fortune_cookie = None
+
+    for i in range(0, 5):
+        try:
+            random_record = random_int(0, len(data) - 1)
+            (start, length) = data[random_record]
+            f = open(fortune_file, 'rt')
+            f.seek(start)
+            fortune_cookie = f.read(length)
+            f.close()
+            break
+        except UnicodeDecodeError as ude:
+            if i > 3:
+                print(i, ude)
+
+    if fortune_cookie is None:
+        return
+
     spaces = "   "
     content = fortune_cookie.strip().replace("\t", spaces)
     cnt = 0
@@ -105,6 +110,126 @@ def get_random_fortune(fortune_file):
         author = "--" + ncs[1]
 
     return quote, author
+
+
+def main():
+    """
+    Main program.
+    """
+    arg_parser = ArgumentParser()
+    arg_parser.add_argument('-q', '--quiet', action='store_true', dest='quiet',
+                            help="When updating the index file, don't emit "
+                            "messages.")
+
+    arg_parser.add_argument('-u', '--update', action='store_true', dest='update',
+                            help='Update the index file, instead of printing a '
+                            'fortune.')
+
+    arg_parser.add_argument('-d', '--fortunefolder', dest='fortunefolder', help='Fortune basedir to use.')
+    arg_parser.add_argument('-f', '--fortunefile', dest='fortunefile', help='Fortune file to use.')
+    arg_parser.add_argument('-r', '--random', dest='random', help='Use random fortune file to use.', action="store_true")
+    arg_parser.add_argument('-l', '--length', dest='length', help='Max length.', action="store")
+    arg_parser.add_argument('-s', '--search', dest='search', help='Search a quote.', action="store")
+    args = arg_parser.parse_args(sys.argv[1:])
+    lf = []
+    length = None
+
+    try:
+        if args.length is not None:
+            length = int(args.length)
+    except ValueError:
+        pass
+
+    if args.fortunefolder is None:
+        print('no fortunefolder given')
+        return
+
+    if args.random is True:
+        for fn in os.listdir(args.fortunefolder):
+            if fn.endswith(".dat"):
+                lf.append(fn.replace(".dat", ""))
+
+    if len(lf) > 0:
+        fortune_file = lf[random.randint(0, len(lf) - 1)]
+    else:
+        fortune_file = args.fortunefile
+
+    fortune_title = ""
+    if fortune_file is not None or args.search:
+        if fortune_file:
+            fortune_title = fortune_file.capitalize()
+            fortune_file = os.path.join(args.fortunefolder, fortune_file)
+
+        quote = None
+        author = ""
+
+        if args.search is not None:
+            if length is None:
+                length = -1
+
+            results = search_fortune(args.search, fortune_file)
+
+            for index in results:
+                for score, fortune_file, quote, author in results[index]:
+                    if score > len(args.search.split(" ")) / 3:
+                        quotelen = len(quote)
+                        if quotelen < length or length == -1:
+                            print("\033[96m" + fortune_file.replace("./quotes/", "").capitalize() + " (" + str(score) + "):\033[0m\n\033[94m" + quote + "\033[94m" + author, "\033[0m\n")
+
+        elif args.update:
+            make_fortune_data_file(fortune_file)
+        else:
+            if length is None:
+                quote, author = get_random_fortune(fortune_file)
+            else:
+                quotelen = -1
+
+                while (quotelen > length) or (quotelen < 0):
+                    quote, author = get_random_fortune(fortune_file)
+                    quotelen = len(quote)
+
+            if quote is not None:
+                print("\033[96m" + fortune_title + ":\033[0m\n\033[94m" + quote + "\033[94m" + author, "\033[0m")
+    else:
+        print('no file given')
+
+
+def make_fortune_data_file(fortune_file, quiet=False):
+    """
+    @type fortune_file: str, unicode
+    @type quiet: bool
+    @return: None
+    """
+    fortune_index_file = fortune_file + '.dat'
+
+    if not quiet:
+        print('Updating "%s" from "%s"...' % (fortune_index_file, fortune_file))
+
+    data = []
+    shortest = sys.maxsize
+    longest = 0
+
+    for start, length, fortune in _read_fortunes(open(fortune_file, 'rt')):
+        data += [(start, length)]
+        shortest = min(shortest, length)
+        longest = max(longest, length)
+
+    fortune_index = open(fortune_index_file, 'wb')
+    pickle.dump(data, fortune_index, _PICKLE_PROTOCOL)
+    fortune_index.close()
+
+    if not quiet:
+        print('Processed %d fortunes.\nLongest: %d\nShortest %d' %
+              (len(data), longest, shortest))
+
+
+def random_int(start, end):
+    """
+    @type start: str, unicode
+    @type end: str, unicode
+    @return: None
+    """
+    return random.randint(start, end)
 
 
 def search_fortune(searchquery, fortunefilep=None):
@@ -168,21 +293,18 @@ def search_fortune(searchquery, fortunefilep=None):
 
                 score = 0
                 quotesplit = [x for x in quote.lower().split(" ") if x]
-
                 for word in qwords:
                     if word in quotesplit:
                         score += 1
 
                 for word in qwords:
                     if word in author.lower():
-
                         if score not in results:
                             results[score] = []
-                        results[score].append((100, fortune_file.replace(".dat", ""), quote, author))
 
+                        results[score].append((100, fortune_file.replace(".dat", ""), quote, author))
                     elif score > 0:
                         if score == len(quotesplit):
-
                             if score not in results:
                                 results[score] = []
 
@@ -195,142 +317,7 @@ def search_fortune(searchquery, fortunefilep=None):
 
     return results
 
-
-def _read_fortunes(fortune_file):
-    """ Yield fortunes as lists of lines """
-    result = []
-    start = None
-    pos = 0
-
-    for line in fortune_file:
-        if line == "%\n":
-            if pos != 0:  # "%" at top of file. Skip it.
-                yield (start, pos - start, result)
-                result = []
-                start = None
-        else:
-            if start is None:
-                start = pos
-
-            result.append(line)
-
-        pos += len(line)
-
-    if result:
-        yield (start, pos - start, result)
-
-
-def make_fortune_data_file(fortune_file, quiet=False):
-    """
-    @type fortune_file: str, unicode
-    @type quiet: bool
-    @return: None
-    """
-    fortune_index_file = fortune_file + '.dat'
-
-    if not quiet:
-        print('Updating "%s" from "%s"...' % (fortune_index_file, fortune_file))
-
-    data = []
-    shortest = sys.maxsize
-    longest = 0
-
-    for start, length, fortune in _read_fortunes(open(fortune_file, 'rt')):
-        data += [(start, length)]
-        shortest = min(shortest, length)
-        longest = max(longest, length)
-
-    fortune_index = open(fortune_index_file, 'wb')
-    pickle.dump(data, fortune_index, _PICKLE_PROTOCOL)
-    fortune_index.close()
-
-    if not quiet:
-        print('Processed %d fortunes.\nLongest: %d\nShortest %d' %
-              (len(data), longest, shortest))
-
-
-def main():
-    """
-    Main program.
-    """
-    from argparse import ArgumentParser
-    arg_parser = ArgumentParser()
-    arg_parser.add_argument('-q', '--quiet', action='store_true', dest='quiet',
-                            help="When updating the index file, don't emit "
-                            "messages.")
-
-    arg_parser.add_argument('-u', '--update', action='store_true', dest='update',
-                            help='Update the index file, instead of printing a '
-                            'fortune.')
-
-    arg_parser.add_argument('-d', '--fortunefolder', dest='fortunefolder', help='Fortune basedir to use.')
-    arg_parser.add_argument('-f', '--fortunefile', dest='fortunefile', help='Fortune file to use.')
-    arg_parser.add_argument('-r', '--random', dest='random', help='Use random fortune file to use.', action="store_true")
-    arg_parser.add_argument('-l', '--length', dest='length', help='Max length.', action="store")
-    arg_parser.add_argument('-s', '--search', dest='search', help='Search a quote.', action="store")
-    args = arg_parser.parse_args(sys.argv[1:])
-    lf = []
-    length = None
-    try:
-        if args.length is not None:
-            length = int(args.length)
-    except ValueError:
-        pass
-
-    if args.fortunefolder is None:
-        print('no fortunefolder given')
-        return
-
-    if args.random is True:
-        for fn in os.listdir(args.fortunefolder):
-            if fn.endswith(".dat"):
-                lf.append(fn.replace(".dat", ""))
-
-    if len(lf) > 0:
-        fortune_file = lf[random.randint(0, len(lf) - 1)]
-    else:
-        fortune_file = args.fortunefile
-
-    fortune_title = ""
-    if fortune_file is not None or args.search:
-        if fortune_file:
-            fortune_title = fortune_file.capitalize()
-            fortune_file = os.path.join(args.fortunefolder, fortune_file)
-
-        quote = None
-        author = ""
-
-        if args.search is not None:
-            if length is None:
-                length = -1
-
-            results = search_fortune(args.search, fortune_file)
-
-            for index in results:
-
-
-                for score, fortune_file, quote, author in results[index]:
-                    if score > len(args.search.split(" ")) / 3:
-                        quotelen = len(quote)
-                        if quotelen < length or length == -1:
-                            print("\033[96m" + fortune_file.replace("./quotes/", "").capitalize() + " ("+str(score) + "):\033[0m\n\033[94m" + quote + "\033[94m" + author, "\033[0m\n")
-
-        elif args.update:
-            make_fortune_data_file(fortune_file)
-        else:
-            if length is None:
-                quote, author = get_random_fortune(fortune_file)
-            else:
-                quotelen = -1
-
-                while (quotelen > length) or (quotelen < 0):
-                    quote, author = get_random_fortune(fortune_file)
-                    quotelen = len(quote)
-
-            if quote is not None:
-                print("\033[96m" + fortune_title + ":\033[0m\n\033[94m" + quote + "\033[94m" + author, "\033[0m")
-    else:
-        print('no file given')
+standard_library.install_aliases()
 
 
 if __name__ == '__main__':
